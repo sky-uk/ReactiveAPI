438930eac45baf3cd29ef88a899a5721853f0b35
Elisa Bordoli <elisa.bordoli@skytv.it>
2019-05-12 19:04:41 +0200

    Code coverage + first test

---
+++ ReactiveAPITests/ExtensionsTests.swift
@@ -0,0 +1,25 @@
+import XCTest
+import ReactiveAPI
+
+class ExtensionsTests: XCTestCase {
+    private struct MockEncodable: Encodable {
+        let name: String
+        let id: Double
+    }
+
+    func test_Dictionary_WhenDataIsValid_RetursDictionary() {
+        let encodable = MockEncodable(name: "Patrick",
+                                      id: 5)
+        let result = encodable.dictionary
+        XCTAssertNotNil(result)
+        XCTAssertEqual(result?["name"] as? String, "Patrick")
+        XCTAssertEqual(result?["id"] as? Int, 5)
+    }
+
+    func test_Dictionary_WhenDataIsNotValid_RetursNil() {
+        let encodable = MockEncodable(name: "Infinity",
+                                      id: Double.infinity)
+        let result = encodable.dictionary
+        XCTAssertNil(result)
+    }
+}

e935bbbdbaeea7a4f0fa635345df66e660fc9514
Aleksandar Gotev <alex@gotev.net>
2019-06-05 12:30:14 +0200

    implemented query string type converter

--- ReactiveAPI/ReactiveAPI.swift
+++ ReactiveAPI/ReactiveAPI.swift
@@ -1,9 +1,12 @@
 import Foundation
 import RxSwift
 
+public typealias ReactiveAPITypeConverter = (_ value: Any?) -> String?
+
 public protocol ReactiveAPI {
     var authenticator: ReactiveAPIAuthenticator? { get set }
     var requestInterceptors: [ReactiveAPIRequestInterceptor] { get set }
+    var queryStringTypeConverter: ReactiveAPITypeConverter? { get set }
     var cache: ReactiveAPICache? { get set }
     init(session: Reactive<URLSession>, decoder: ReactiveAPIDecoder, baseUrl: URL)
     func absoluteURL(_ endpoint: String) -> URL

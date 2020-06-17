88fdb1e0d0de481a2dd0526737818355f9f2a12c
Chiarotto Alessandro <alessandro.chiarotto.dechit@skytv.it>
2020-06-17 16:47:05 +0200

    Improved Test Suite

--- ReactiveAPITests/Mocks/JSONHelper.swift
+++ ReactiveAPITests/Mocks/JSONHelper.swift
@@ -6,20 +6,20 @@ public class JSONHelper {
         case inconsitency
     }
 
-    public static func stubError() -> OHHTTPStubsResponse {
-        return OHHTTPStubsResponse(error: StubError.inconsitency)
+    public static func stubError() -> HTTPStubsResponse {
+        return HTTPStubsResponse(error: StubError.inconsitency)
     }
     static private let jsonContentType = ["Content-Type": "application/json"]
 
-    public static func jsonHttpResponse<T: Encodable>(value: T) throws -> OHHTTPStubsResponse {
+    public static func jsonHttpResponse<T: Encodable>(value: T) throws -> HTTPStubsResponse {
         let json = try JSONHelper.encode(value: value)
-        return OHHTTPStubsResponse(data: json,
+        return HTTPStubsResponse(data: json,
                                    statusCode: 200,
                                    headers: jsonContentType)
     }
 
-    public static func unauthorized401() -> OHHTTPStubsResponse {
-        return OHHTTPStubsResponse(data: Data(), statusCode: 401, headers: [:])
+    public static func unauthorized401() -> HTTPStubsResponse {
+        return HTTPStubsResponse(data: Data(), statusCode: 401, headers: [:])
     }
 
     public static func encode<T: Encodable>(value: T) throws -> Data {

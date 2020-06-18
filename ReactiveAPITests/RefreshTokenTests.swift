998362a6401ceb8ee4a8120069675d5d7044fbb3
Chiarotto Alessandro <alessandro.chiarotto.dechit@skytv.it>
2020-06-18 17:50:55 +0200

    Update UT

--- ReactiveAPITests/RefreshTokenTests.swift
+++ ReactiveAPITests/RefreshTokenTests.swift
@@ -2,6 +2,7 @@ import XCTest
 import RxSwift
 import Swifter
 @testable import ReactiveAPI
+
 class RefreshTokenTests: SkyTestCase {
 
     private let authenticator = ReactiveAPITokenAuthenticator(tokenHeaderName: "tokenHeaderName",
@@ -28,29 +29,27 @@ class RefreshTokenTests: SkyTestCase {
             sut.requestInterceptors += [ TokenInterceptor(tokenValue: { return currentToken }, headerName: tokenHeaderName) ]
 
             httpServer.route(ClientAPI.Endpoint.login) { (request, callCount) -> (HttpResponse) in
-                XCTAssertLessThanOrEqual(callCount, 2)
-                switch callCount {
-                    case 1:
-                        XCTAssertEqual(request.header(name: tokenHeaderName), token1)
-                        return HttpResponse.ok(Model.mock().encoded())
-                    default:
-                        XCTFail("switch endLoginCall")
-                }
-                return HttpResponse.unauthorized
+                XCTAssertEqual(callCount, 1)
+                XCTAssertEqual(request.header(name: tokenHeaderName), token1)
+                return HttpResponse.ok(Model.mock().encoded())
             }
 
-            httpServer.route(ClientAPI.Endpoint.renew) { (_, callCount) -> (HttpResponse) in
-                XCTAssertLessThanOrEqual(callCount, 1)
+            httpServer.route(ClientAPI.Endpoint.renew) { (request, callCount) -> (HttpResponse) in
+                print("Request:\(request.path)  Thread.current:\(Thread.current)")
+                XCTAssertEqual(callCount, 1)
                 return HttpResponse.ok(Model.mock(name: token2).encoded())
             }
 
             var endpointCallCountToken2 = 0
+            var endpointCallCountToken1 = 0
             httpServer.route("/end-point/:path/call") { (request, callCount) -> (HttpResponse) in
+                print("Request:\(request.path)  Thread.current:\(Thread.current)")
                 let token = request.header(name: tokenHeaderName)
                 XCTAssertNotNil(token)
                 XCTAssertTrue(callCount <= 2 ? token == token1 : token == token2)
                 switch token {
                     case token1:
+                        endpointCallCountToken1 += 1
                         sleep(callCount == 1 ? 4 : 0)
                         return HttpResponse.unauthorized
                     case token2:
@@ -74,6 +73,7 @@ class RefreshTokenTests: SkyTestCase {
             // Then
             XCTAssertNotNil(events)
             XCTAssertEqual(endpointCallCountToken2, 2)
+            XCTAssertEqual(endpointCallCountToken1, 2)
         } catch {
             XCTFail("\(error)")
         }

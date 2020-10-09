51437d7d433626f3fef1be9be0403de5e943afcd
Chiarotto Alessandro <alessandro.chiarotto.dechit@skytv.it>
2020-10-09 14:39:51 +0200

    ReactiveDemo xcodegen & and spm

--- ReactiveAPI/ReactiveAPIAuthenticator.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import RxSwift
-
-public protocol ReactiveAPIAuthenticator {
-    func authenticate(session: Reactive<URLSession>,
-                      request: URLRequest,
-                      response: HTTPURLResponse,
-                      data: Data?) -> Single<Data>?
-}
--- ReactiveAPI/ReactiveAPIRequestInterceptor.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-public protocol ReactiveAPIRequestInterceptor {
-    func intercept(_ request: URLRequest) -> URLRequest
-}

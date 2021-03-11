222f4ed8da25b7bc89c95a33142c3a2edf6d67f3
Valeria Alampi <valeria.alampi@skytv.it>
2021-03-11 15:06:16 +0100

    new combine tests

--- Tests/ReactiveAPITests/JSONReactiveAPITests.swift
+++ Tests/ReactiveAPITests/JSONReactiveAPITests.swift
@@ -10,9 +10,19 @@ class JSONReactiveAPITests: XCTestCase {
                            baseUrl: Resources.baseUrl)
     }
 
+    private var api1: ReactiveAPI {
+        return ReactiveAPI(session: session,
+                           baseUrl: Resources.baseUrl)
+    }
+
     func test_Init_JSONReactiveAPI() {
         XCTAssertEqual(api.session.base, session)
         XCTAssertNotNil(api.decoder)
+    }
+
+    func test_Init_JSONReactiveAPI_Combine() {
+        XCTAssertEqual(api1.session1, session)
+        XCTAssertNotNil(api1.decoder)
     }
 
     func test_AbsoluteURL_AppendsEndpoint() {
@@ -20,8 +30,18 @@ class JSONReactiveAPITests: XCTestCase {
         XCTAssertEqual(url.absoluteString, "http://www.mock.com/path")
     }
 
+    func test_AbsoluteURL_AppendsEndpoint_Combine() {
+        let url = api1.absoluteURL("path")
+        XCTAssertEqual(url.absoluteString, "http://www.mock.com/path")
+    }
+
     func test_AbsoluteURL_AppendsEmptyEndpoint() {
         let url = api.absoluteURL("")
+        XCTAssertEqual(url.absoluteString, "http://www.mock.com/")
+    }
+
+    func test_AbsoluteURL_AppendsEmptyEndpoint_Combine() {
+        let url = api1.absoluteURL("")
         XCTAssertEqual(url.absoluteString, "http://www.mock.com/")
     }
 }
